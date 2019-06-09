abd4840df9fb4ec637d736c0d880cc215cfb8b5b
jferrier <jferrier@15.2.0.10.rev.sfr.net>
2019-06-09 15:39:43 -0400

    [added] bomberman.hpp

---
+++ inc/bomberman.hpp
@@ -0,0 +1,38 @@
+//
+// bomberman.hpp for  in /home/hazer/cpp_indie_studio/include
+//
+// 
+// made by <julien.ferrier@epitech.eu>
+//
+//
+//
+//
+
+#ifndef BOMBERMAN_HPP
+# define BOMBERMAN_HPP
+
+#define	BOMBER_OK	0
+#define	BOMBER_ERR	-1
+
+#define RED		"\033[1;31m"
+#define GREEN           "\033[1;32m"
+#define PURPLE          "\033[1;35m"
+#define CYAN            "\033[1;36m"
+#define BROWN           "\033[0;33m"
+#define BLUE            "\033[1;34m"
+#define YELLOW          "\033[1;33m"
+#define BLANK		"\033[0m"
+
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+#include <vector>
+#include <thread>
+#include <string>
+#include <mutex>
+#include <limits>
+#include <time.h>
+#include <sstream>
+
+#endif /* !_BOMBERMAN_HPP_ */
