a419c260b08cfd577af6523647b0f47f49084081
Hugo Allabert <hugo.allabert@epitech.eu>
2019-06-10 03:52:46 +0200

    ADD(inc/Globals.hpp): Add Globals.hpp, globals structs and enums.

---
+++ inc/Globals.hpp
@@ -0,0 +1,57 @@
+//
+// EPITECH PROJECT, 2018
+// OOP_indie_studio_2018
+// File description:
+// Globals.hpp
+//
+
+#ifndef GLOBALS_HPP_
+#define GLOBALS_HPP_
+
+#include <irrlicht.h>
+#include <map>
+#include <utility>
+#include <chrono>
+#include <ctime>
+#include <irrKlang.h>
+#include <iostream>
+
+enum Window_Type {
+	FSCREEN,
+	MENU,
+	SAVES,
+	SETTINGS,
+	GAME,
+	HIGHSCORE,
+	HELP,
+	PAUSE,
+	P1WIN,
+	P2WIN,
+	IAWIN,
+	END_GAME
+};
+
+enum Rotation {
+	RIGHT,
+	LEFT,
+	UP,
+	DOWN
+};
+
+typedef struct	s_global_data {
+	irr::IrrlichtDevice		*device;
+	irr::video::IVideoDriver	*driver;
+	irr::scene::ISceneManager	*sceneMngr;
+	irrklang::ISoundEngine		*soundEng;
+	irr::gui::IGUIEnvironment	*guiEnv;
+        Window_Type			winType;
+        bool				inMenu;
+        bool				inGame;
+	bool				quit;
+	bool				sound;
+	std::map			<irr::s32, std::pair<irr::s32, irr::s32>> select_pos;
+	irr::s32				i;
+	irr::s32				nbPlayers;
+}		t_global_data;
+
+#endif
